19214cd57ff35f1c229344f1c800545fe5de4b5b
LeandroSantosP <skp.Tridimensional@gmail.com>
2023-01-13 00:56:20 -0300

    fix: change url

--- src/shared/services/axios-config/index.ts
+++ src/shared/services/axios-config/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { errorInterceptor, ResponseInterceptor } from "./interceptors";
 //https://json-server-3huybff4y-leandrosantosp.vercel.app
+//https://localhost:3000
 const BASE_URL = "https://json-server-ebon.vercel.app/"
 
 export const Api = axios.create({
