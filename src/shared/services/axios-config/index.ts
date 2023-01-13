e4cdc34a28319e7fe4cb893cb371b29d618e52af
Leandro Pereira Dos Santos <57951558+LeandroSantosP@users.noreply.github.com>
2023-01-12 22:04:03 -0300

    Update index.ts

--- src/shared/services/axios-config/index.ts
+++ src/shared/services/axios-config/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { errorInterceptor, ResponseInterceptor } from "./interceptors";
 
-const BASE_URL = "https://json-server-ebon.vercel.app"
+const BASE_URL = "https://json-server-ebon.vercel.app/"
 
 export const Api = axios.create({
    baseURL: BASE_URL
